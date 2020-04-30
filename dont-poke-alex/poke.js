e50641ab497d741e64237acc69f17a5fbdfb2add
ameddin73 <ameddin73@gmail.com>
2020-04-30 00:16:44 -0400

    poke is mostly done just needs assets

--- dont-poke-alex/poke.js
+++ dont-poke-alex/poke.js
@@ -1,7 +1,8 @@
 var myGamePiece;
+const src = "assets/research-dog.png";
 
 function startGame() {
-    myGamePiece = new component(100, 100, 1, "public/research-dog.png");
+    myGamePiece = new component(100, 100, 2);
     this.interval = setInterval(updateGameArea, 20);
 }
 
@@ -12,35 +13,35 @@ const getCanvas = function () {
     }
 }
 
-const getNumber = function (position, incr) {
-    console.log(position)
-    let value = parseInt(position.match(/(\d+)/), 10);
-    value += incr;
-    console.log(value)
-    return value + "px";
-}
-
-function component(width, height, speed, src) {
+function component(width, height, speed) {
     this.width = width;
     this.height = height;
     this.speedX = speed;
     this.speedY = speed;
     this.x = 0;
     this.y = 0;
-    var alex = new Image(width, height);
+
+    const alex = new Image(width, height);
+    const sfx = document.createElement("AUDIO");
+
     alex.src = src;
     alex.className = "alex";
-    alex.onclick = () => {
-        console.log('nope')
-        alert('huh')
+    alex.onclick = async function () {
+        sfx.setAttribute("src", sfxs[Math.floor(Math.random() * sfxs.length)])
+        sfx.play();
+        alex.src = srcs[Math.floor(Math.random() * srcs.length)];
+        await sleep(300)
+        alex.src = src;
     }
+
+    document.body.appendChild(sfx);
     document.body.appendChild(alex);
+
     this.update = function () {
-        this.x += this.speedX;
-        this.y += this.speedY;
         alex.style.left = this.x + "px";
         alex.style.top = this.y + "px";
     };
+
     this.collide = function () {
         // Bottom
         if (this.y + this.height >= getCanvas().height) {
@@ -53,33 +54,41 @@ function component(width, height, speed, src) {
         // Right
         if (this.x + this.width >= getCanvas().width) {
             this.speedX = -1 * this.speedX;
+            alex.classList.add("faceLeft");
         }
         // Left
         if (this.x <= 0) {
             this.speedX = -1 * this.speedX;
+            alex.classList.remove("faceLeft");
         }
     };
+
     this.outOfBounds = function () {
         // Bottom
         if (this.y > getCanvas().height) {
             startGame();
+            updateGameArea();
             return;
         }
         // Top
         if (this.y < 0 - this.height) {
             startGame();
+            updateGameArea();
             return;
         }
         // Right
         if (this.x > getCanvas().width) {
             startGame();
+            updateGameArea();
             return;
         }
         // Left
         if (this.x < 0 - this.width) {
             startGame();
+            updateGameArea();
         }
     };
+
     this.newPos = function () {
         this.x += this.speedX;
         this.y += this.speedY;
@@ -92,3 +101,14 @@ function updateGameArea() {
     myGamePiece.collide();
     myGamePiece.update();
 }
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const srcs = [
+    "assets/research-dog-boom.png",
+]
+const sfxs = [
+    "assets/poke.mp3",
+]
